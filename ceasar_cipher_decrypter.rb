06a1f9b85147fe45179b4556e41d3898611cee90
viczinoviev <106315428+viczinoviev@users.noreply.github.com>
2025-12-03 15:49:25 -0500

    Initial commit

---
+++ ceasar_cipher_decrypter.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CeasarCipherDecrypter
+  NUMBER_OF_ALPHABET = ('a'..'z').to_a.length
+
+  def brute_force(string)
+    lower_cased_string = string.downcase
+    (0..NUMBER_OF_ALPHABET - 1).each do |i|
+      shifted_string = lower_cased_string.chars.map do |character|
+        shifted_ascii_value = (character.ord + i)
+        if shifted_ascii_value > 'z'.ord
+          shifted_ascii_value -= NUMBER_OF_ALPHABET
+        end
+        shifted_ascii_value.chr
+      end.join
+      puts shifted_string
+    end
+  end
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative 'ceasar_cipher_decrypter'
+
+decrypter = CeasarCipherDecrypter.new
+decrypter.brute_force('BTTIBTPIIWTHTRGTIADRPIXDC')
